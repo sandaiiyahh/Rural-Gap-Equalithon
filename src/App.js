e36a508c80fa439f8c19015a4ad9297e6d071739
Sandy Dai <sandydai@sandys-imac.myfiosgateway.com>
2021-05-26 12:06:53 -0400

    adds MediaSelect route component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
-// import { Router, Redirect, Route, Switch } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from 'react-router-dom';
 // import Webcam from 'react-webcam';
 import Permissions from './components/Permissions';
+import MediaSelect from './components/MediaSelect';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      {
-        /* <Webcam /> */
-        <Permissions />
-      }
+      <Router>
+        <Switch>
+          {/* <Webcam /> */}
+          <Route exact path="/" component={Permissions} />
+          <Route path="/start" component={MediaSelect} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/MediaSelect.js
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+
+function MediaSelect() {
+  return <div>Hi</div>;
+}
+
+export default MediaSelect;
