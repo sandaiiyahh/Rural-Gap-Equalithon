4a8db4ddf069bee1edf6b5278d0948825753d633
Sandy Dai <sandydai@sandys-imac.myfiosgateway.com>
2021-05-26 12:43:10 -0400

    style MediaSelect question prompt

--- src/components/MediaSelect.js
+++ src/components/MediaSelect.js
@@ -2,21 +2,24 @@ import React, { useState } from 'react';
 
 function MediaSelect() {
   return (
-    <>
-      <h1>Question:</h1>
-      <p>
+    <div class="container">
+      <p class="question">Question:</p>
+      <p class="prompt">
         Lorem ipsum dolor sit amet, consectur adipicing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua?
       </p>
-      <p>1.</p>
-      <p>Choose the recording method.</p>
-      <p>2.</p>
-      <p>Click to start button to record.</p>
-      <p>3.</p>
-      <p>Try recording again or submit it.</p>
+      <div class="steps">
+        <p class="number">1.</p>
+        <p class="step-text">Choose the recording method.</p>
+
+        <p class="number">2.</p>
+        <p class="step-text">Click to start button to record.</p>
+        <p class="number">3.</p>
+        <p class="step-text">Try recording again or submit it.</p>
+      </div>
       <button>Video Recording</button>
       <button>Voice Recording</button>
-    </>
+    </div>
   );
 }
 
