f0b74633d4581db4a8aa5e3a3b927abfa592d4a4
Sandy Dai <sandydai@sandys-imac.myfiosgateway.com>
2021-05-26 12:17:19 -0400

    adds MediaSelect content

--- src/components/MediaSelect.js
+++ src/components/MediaSelect.js
@@ -1,7 +1,23 @@
 import React, { useState } from 'react';
 
 function MediaSelect() {
-  return <div>Hi</div>;
+  return (
+    <>
+      <h1>Question:</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectur adipicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua?
+      </p>
+      <p>1.</p>
+      <p>Choose the recording method.</p>
+      <p>2.</p>
+      <p>Click to start button to record.</p>
+      <p>3.</p>
+      <p>Try recording again or submit it.</p>
+      <button>Video Recording</button>
+      <button>Voice Recording</button>
+    </>
+  );
 }
 
 export default MediaSelect;
