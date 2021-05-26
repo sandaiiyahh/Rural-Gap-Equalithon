93bee1220d98eec57ae1d5a4241c65d6e1ac980a
Sandy Dai <sandydai@sandys-imac.myfiosgateway.com>
2021-05-26 17:20:04 -0400

    adds mic icon to voice recording

--- src/components/MediaSelect.js
+++ src/components/MediaSelect.js
@@ -2,23 +2,29 @@ import React, { useState } from 'react';
 
 function MediaSelect() {
   return (
-    <div class="container">
-      <p class="question">Question:</p>
-      <p class="prompt">
+    <div className="container">
+      <p className="question">Question:</p>
+      <p className="prompt">
         Lorem ipsum dolor sit amet, consectur adipicing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua?
       </p>
-      <div class="steps">
-        <p class="number">1.</p>
-        <p class="step-text">Choose the recording method.</p>
+      <div className="steps">
+        <p className="number">1.</p>
+        <p className="step-text">Choose the recording method.</p>
 
-        <p class="number">2.</p>
-        <p class="step-text">Click to start button to record.</p>
-        <p class="number">3.</p>
-        <p class="step-text">Try recording again or submit it.</p>
+        <p className="number">2.</p>
+        <p className="step-text">Click to start button to record.</p>
+        <p className="number">3.</p>
+        <p className="step-text">Try recording again or submit it.</p>
       </div>
-      <button class="media-btn">Video Recording</button>
-      <button class="media-btn">Voice Recording</button>
+      <button className="media-btn">
+        <i className="bi bi-camera-video icon"></i>
+        <p className="btn-text">Video Recording</p>
+      </button>
+      <button className="media-btn">
+        <i className="bi bi-mic mic"></i>
+        <p className="btn-text">Voice Recording</p>
+      </button>
     </div>
   );
 }
