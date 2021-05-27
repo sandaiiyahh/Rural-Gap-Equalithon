ccb832f8ee661c158cddbccf351e1e502e438b56
Abigail Mathews <abbymathews@gmail.com>
2021-05-26 22:25:00 -0400

    Header component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-// import Webcam from 'react-webcam';
+import Header from './components/Header';
 import Permissions from './components/Permissions';
 import MediaSelect from './components/MediaSelect';
 import './App.css';
@@ -8,9 +8,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <Header />
       <Router>
         <Switch>
-          {/* <Webcam /> */}
           <Route exact path="/" component={Permissions} />
           <Route path="/start" component={MediaSelect} />
         </Switch>
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Navbar } from 'react-bootstrap';
+
+function Header() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="#home">America Talks</Navbar.Brand>
+    </Navbar>
+  );
+}
+
+export default Header;
