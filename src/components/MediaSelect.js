7ee990fa56d7c8c1cd50ebc0832e44aa48bb9096
Sandy Dai <sandydai@sandys-imac.myfiosgateway.com>
2021-05-27 21:55:11 -0400

    fix: remove state from MediaSelect

--- src/components/MediaSelect.js
+++ src/components/MediaSelect.js
@@ -1,53 +1,52 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
-import Record from './Record';
 
 function MediaSelect(props) {
-  console.log('props --->', props);
-  const [showSteps, setShowSteps] = useState(true);
-
-  function handleClick(e) {
-    setShowSteps(false);
-    if (e.target.innerText.includes('Audio')) props.setMediaType('A');
-    console.log(props.mediaType);
-  }
   return (
     <div>
-      {showSteps && (
-        <div>
-          <div className="row text-align-center steps">
-            <div className="col-10 col-sm-4">
-              <p className="number">1.</p>
-              <p className="step-text">Choose the recording method.</p>
-            </div>
-
-            <div className="col-10 col-sm-4">
-              <p className="number">2.</p>
-              <p className="step-text">Click to start button to record.</p>
-            </div>
-
-            <div className="col-10 col-sm-4">
-              <p className="number">3.</p>
-              <p className="step-text">Try recording again or submit it.</p>
-            </div>
+      <div>
+        <div className="row text-align-center steps">
+          <div className="col-10 col-sm-4">
+            <p className="number">1.</p>
+            <p className="step-text">Choose the recording method.</p>
           </div>
-          <div className="row justify-content-center">
-            <div className="col-6 col-md-4">
-              <Button className="media-btn" onClick={(e) => handleClick(e)}>
+
+          <div className="col-10 col-sm-4">
+            <p className="number">2.</p>
+            <p className="step-text">Click to start button to record.</p>
+          </div>
+
+          <div className="col-10 col-sm-4">
+            <p className="number">3.</p>
+            <p className="step-text">Try recording again or submit it.</p>
+          </div>
+        </div>
+        <div className="row justify-content-center">
+          <div className="col-6 col-md-4">
+            <Link to="/record">
+              <Button
+                className="media-btn"
+                onClick={() => props.setMediaType('V')}
+              >
                 <i className="bi bi-camera-video icon"></i>
                 <p className="btn-text">Video Recording</p>
               </Button>
-            </div>
-            <div className="col-6 col-md-4">
-              <Button className="media-btn" onClick={(e) => handleClick(e)}>
+            </Link>
+          </div>
+          <div className="col-6 col-md-4">
+            <Link to="/record">
+              <Button
+                className="media-btn"
+                onClick={() => props.setMediaType('A')}
+              >
                 <i className="bi bi-mic mic"></i>
                 <p className="btn-text">Voice Recording</p>
               </Button>
-            </div>
+            </Link>
           </div>
         </div>
-      )}
-      {!showSteps && <Record />}
+      </div>
     </div>
   );
 }
