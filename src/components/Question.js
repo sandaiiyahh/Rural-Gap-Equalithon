f1285b991e463969fd45cd64293cf5606de4adee
Sandy Dai <sandydai@sandys-imac.myfiosgateway.com>
2021-05-29 17:58:47 -0400

    style(Question): fix minor positioning

--- src/components/Question.js
+++ src/components/Question.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 
 function Question(props) {
-  console.log('props -->', props);
   return (
     <Container
       className={`${
