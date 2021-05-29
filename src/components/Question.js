4027d1d0237a7bff4b68779fd1740915a8c696f8
Sandy Dai <sandydai@sandys-imac.myfiosgateway.com>
2021-05-29 14:38:33 -0400

    style(Question): center Question

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 
-function Question(props) {
+function Question() {
   return (
-    <Container className="p-5">
-      <p className="question">Question:</p>
-      <p className="prompt mb-0">
-        Lorem ipsum dolor sit amet, consectur adipicing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua?
+    <Container className="p-5 question-container">
+      <p className="question mr-5 text-center">Question</p>
+      <p className="prompt mb-0 text-center">
+        <span className="bold">In 30 seconds max</span>, Lorem ipsum dolor sit
+        amet, consectur adipicing elit, sed do eiusmod tempor incididunt ut
+        labore et dolore magna aliqua?
       </p>
     </Container>
   );
